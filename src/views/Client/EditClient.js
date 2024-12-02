84d25db261bf9b8a219f2add46e09e9f3d145e5a
MianAteeq <31040996+MianAteeq@users.noreply.github.com>
2024-12-02 18:46:12 +0500

    Update EditClient.js

--- src/views/Client/EditClient.js
+++ src/views/Client/EditClient.js
@@ -106,7 +106,11 @@ const EditClient = (contact) => {
 
     })
     if (errors) {
-      setError(errors[0].message)
+     if (errors[0].errorType === 'DynamoDB:ConditionalCheckFailedException') {
+        setError('Phone Number Already Exist')
+      }else{
+       setError(errors[0].message)
+      }
     } else {
 
       navigate(-1)
